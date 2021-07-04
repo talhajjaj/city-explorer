d01a3e2d7dbea89197c6d2c7a13a32784208158c
talhajjaj <taima.hajaj@gmail.com>
2021-07-04 22:19:30 +0300

    trying to solve the lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import {Button , Form} from "react-bootstrap";
+// npm install dotenv
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData:{},
+      searchQuery:'',
+      showMap: false
+    
+    };
+  }
+
+  getLocation = async (e) => {
+    e.preventDefault();
+
+  
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+        <Button onClick={this.getLocation}>Explore</Button>
+        <Form onSubmit={this.getLocation}>
+          <input type='text' placeholder='city name' name='city'/>
+        </Form>
+
+        <p>City Name: {this.state.cityData.display_name},{this.state.cityData.lat},{this.state.cityData.lon}</p>
+
+        {this.state.showMap && 
+        <img alt='' src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPOLRER_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=1-18`} />
+        }      
+        
+      </div>
+    )
+  }
 }
 
 export default App;
