8fd7e84b99dc67ec5028e8a9f2a9d69bb53ff3e0
talhajjaj <taima.hajaj@gmail.com>
2021-07-07 17:17:12 +0300

    lab7

--- src/App.js
+++ src/App.js
@@ -1,97 +1,103 @@
 import React from 'react';
-import axios from 'axios';
-import {Button , Form} from "react-bootstrap";
+// import axios from 'axios';
+// import  Form from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Weather from './component/weather';
 
 
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      cityData:'',
-      searchQuery:'',
-      showMap: false,
-      ErrorMessage: false,
-      showWeather:false,
-      searchData:'', 
-    }
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     cityData:'',
+  //     searchQuery:'',
+  //     showMap: false,
+  //     ErrorMessage: false,
+  //     showWeather:false,
+  //     searchData:[], 
+  //   }
+  // }
 
-  getLocation = async (event) => {
-    event.preventDefault();
+//   getLocation = async (event) => {
+//     event.preventDefault();
 
-    // await this.setState({
-    //   searchQuery: e.target.city.value
-    // })
+//     // await this.setState({
+//     //   searchQuery: e.target.city.value
+//     // })
 
-    console.log('',this.state.searchQuery)
+//     console.log('',this.state.searchQuery)
 
-    let url = `https://eu1.locationiq.com/v1/search.php?key=pk.370701c55ed7503519f7418b1098f8d2&q=${this.state.searchQuery}&format=json`;
+//     let url = `https://eu1.locationiq.com/v1/search.php?key=pk.370701c55ed7503519f7418b1098f8d2&q=${this.state.searchQuery}&format=json`;
     
-    try {
-      let resData = await axios.get(url);
-      console.log(resData)
-      console.log(resData.data)
-      console.log(resData.data[0])
+//     try {
+//       let resData = await axios.get(url);
+//       console.log(resData)
+//       console.log(resData.data)
+//       console.log(resData.data[0])
   
-      this.setState({
-        cityData: resData.data[0],
-        showMap:true
+//       this.setState({
+//         cityData: resData.data[0],
+//         showMap:true
         
-      })
- let weatherUrl =`http://localhost:3001/`
-      let resWeather = await axios.get(url);
-      console.log(resWeather)
-      console.log(resWeather.data)
-      console.log(resWeather.data[0])
+//       })
+//  let weatherUrl =`https://localhost:3001/weatherInfo?searchData=${this.state.searchQuery}&lat=40.7484284&lon=-73.9856546198733`
+//       let resWeather = await axios.get(weatherUrl);
+//       console.log(resWeather)
+//       console.log(resWeather.data)
+//       console.log(resWeather.data[0])
   
-      this.setState({
-        cityData: resData.data[0],
-        showMap:true
+//       this.setState({
+//         cityData: resData.data[0],
+//         showMap:true,
+//         ErrorMessage:false,
+//         searchData:resWeather.data
         
-      })
-    } catch {
-      this.setState({
-        ErrorMessage: true,
-        showMap:false
-      })
-    }
+        
+//       })
+//       console.log (this.state.searchData)
+      
+//     } catch {
+//       this.setState({
+//         ErrorMessage: true,
+//         showMap:false
+//       })
+//     }
 
-  }
+//   }
   
 
-  updateSearch= (event)=>{
-this.setState({
-        searchQuery: event.target.value
-      })
-  }
+//   updateSearch= (event)=>{
+// this.setState({
+//         searchQuery: event.target.value
+//       })
+//   }
 
 
 
   render() {
     return (
       <div>
-        <h1 style={{textAlign:"center"}}>City Explorer </h1>
+        {/* <h1 style={{textAlign:"center"}}>City Explorer </h1>
         {/* <Button onClick={this.getLocation}>Explore</Button> */}
-        <Form onSubmit={this.getLocation}style={{ width: "60%", marginLeft: "10%", marginRight: "40%" , marginTop:"5%"}}>
+        {/* <Form onSubmit={this.getLocation}style={{ width: "60%", marginLeft: "10%", marginRight: "40%" , marginTop:"5%"}}>
           <input type='text' placeholder='city name' name='city'onChange={this.updateSearch}/> 
-          <input type='submit' value='Explore'
-  />
-        </Form>
+          <input type='submit' value='Explore' */}
+  {/* /> */}
+        {/* </Form> */}
 
-        <p style={{ marginLeft: "10%", marginRight: "40%", marginTop:"2%",borderBlockStyle:'outset'}}>City Name: {this.state.cityData.display_name}
-        <br></br>
-        <br></br>
-
-
-        latitude :{this.state.cityData.lat}
-        <br></br>
-        <br></br>
+        {/* // <p style={{ marginLeft: "10%", marginRight: "40%", marginTop:"2%",borderBlockStyle:'outset'}}>City Name: {this.state.cityData.display_name}
+        // <br></br>
+        // <br></br>
 
 
-        longitude :{this.state.cityData.lon}</p>
+        // latitude :{this.state.cityData.lat}
+        // <br></br>
+        // <br></br> */}
+
+
+        {/* longitude :{this.state.cityData.lon}</p>
 
          {this.state.showMap && 
         <img alt='' src={`https://maps.locationiq.com/v3/staticmap?key=pk.370701c55ed7503519f7418b1098f8d2&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=1-18`} />
@@ -101,7 +107,15 @@ this.setState({
         <p>something went wrong in getting data from locationiq</p>
         }
         
+
+      <p> city name:{this.state.cityData.display_name}</p>
+      {/* <Weather searchData={this.state.searchData}/> */}
+      {/* {this.state.searchData.map(weatherData => { */}
+          {/* // return <Weather description={weatherData.description} date={weatherData.date} />})} */} 
+
       </div>
+      
+      
     )
   }
 }
